ca11f84f7785fe3b46fefcd9cc8f400106d8e8f7
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-12-09 21:13:49 +0100

    part2...thank you ThePrimeagen!

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,57 @@
 import * as fs from "fs";
 import * as path from "path";
 
-const input: string[] = fs
-  .readFileSync(path.resolve(__dirname, "day9.txt"), "utf8")
-  .trim()
-  .split("\n");
+function isInBound(data: number[][], h: number, w: number): Boolean {
+  const width: number = data[0].length;
+  const height: number = data.length;
 
-let sum: number = 0;
-let lowestList: number[] = [];
-let basinList: number[][] = []
-
-for (let i = 0; i < input.length; i++) {
-  for (let j = 0; j < input[i].length; j++) {
-    let current: number = +input[i][j];
-    let right: number = input[i][j + 1] ? +input[i][j + 1] : Infinity;
-    let left: number = input[i][j - 1] ? +input[i][j - 1] : Infinity;
-    let up: number = input[i - 1] ? +input[i - 1][j] : Infinity;
-    let down: number = input[i + 1] ? +input[i + 1][j] : Infinity;
-
-    let lowest = Math.min(current, right, left, up, down);
-
-    if (current === lowest && lowest !== 9) {
-      lowestList.push(lowest);
-      lowest = lowest + 1;
-      sum += lowest;
-    }
-  }
+  return h >= 0 && h < height && w >= 0 && w < width;
 }
 
-console.log(sum, lowestList);
+function isWalkable(data: number[][], h: number, w: number): Boolean {
+  return isInBound(data, h, w) && data[h][w] < 9;
+}
+
+function walk(data: number[][], h: number, w: number): number {
+  if (!isWalkable(data, h, w)) {
+    return 0;
+  }
+
+  data[h][w] = 9;
+  let dirs = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
+
+  return (
+    1 +
+    dirs.reduce(function (sum, dir) {
+      return sum + walk(data, h + dir[0], w + dir[1]);
+    }, 0)
+  );
+}
+
+function main() {
+  const data: number[][] = fs
+    .readFileSync(path.resolve(__dirname, "day9.input"), "utf8")
+    .trim()
+    .split("\n")
+    .map((line) => line.split("").map(Number));
+  const width = data[0].length;
+  const height = data.length;
+  let basinCollection: number[] = [];
+
+  console.log(width, height);
+
+  for (let h = 0; h < height; h++) {
+    for (let w = 0; w < width; w++) {
+      basinCollection.push(walk(data, h, w));
+    }
+  }
+
+  console.log(basinCollection.sort((a, b) => b - a).splice(0, 3).reduce((a, b) => a * b));
+}
+
+main();
